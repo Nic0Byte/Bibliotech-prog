b57074cfff3e38650cb2bd70ba2efdb7efdae601
Nicoló <48124723+Nic0Byte@users.noreply.github.com>
2025-02-08 20:14:25 +0100

    Add files via upload

--- Controller/Controller.h
+++ Controller/Controller.h
@@ -1,36 +1,36 @@
 #pragma once
 
-#include "Observer.h"
 #include "Biblioteca.h"
 #include "View.h"
-
+#include "Observer.h"
 #include <QtCore/QObject>
 
+/// Il Controller eredita da Observer e Subject (oltre a QObject)
 class Controller: public QObject, public Observer, public Subject {
-	Q_OBJECT
+    Q_OBJECT
+public:
+    Controller(ModelPtr model, ViewPtr view)
+        : pModel(model), pView(view)
+    { }
 
-	public:
+    void init();
 
-		Controller(ModelPtr model, ViewPtr view): pModel(model), pView(view) { }
-		
-		void init();
-		void addMusicAlbum(); //
-	
-	protected:
+protected:
+    // Override di notifyAll() per trasmettere il filtro di ricerca
+    virtual void notifyAll() override {
+        // Notifica tutti gli osservatori (es. la View), passando l'indirizzo di mQuery
+        for (Observer* observer : mObservers) {
+            if (observer != this) {
+                observer->update(&mQuery);
+            }
+        }
+    }
 
-		virtual void notifyAll() override {
-			for (Observer* observer : mObservers)
-				observer->update(&mQuery);
-		}
+    // Il metodo update del Controller non deve eseguire alcuna azione per evitare loop
+    virtual void update(void* data) override { (void)data; }
 
-        virtual void update(void*) override { notifyAll(); }
-
-
-	private:
-		
-		ModelPtr pModel;
-		ViewPtr pView;
-
-		QString mQuery;
-
+private:
+    ModelPtr pModel;
+    ViewPtr pView;
+    QString mQuery;
 };
